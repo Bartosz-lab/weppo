de13cfb180806e6abd416f135838ccc9f22b8bc1
Xelonidas <mipokoj@gmail.com>
2022-01-24 19:13:10 +0100

    Widok produktu z możliwością edycji

--- routes/product.js
+++ routes/product.js
@@ -20,5 +20,26 @@ router.get('/:id',  async (req, res) => {
      const render_obj = {};
      render_obj.product = await database.get_products_by_category(req.params.id);
      render_obj.proporties = await database.get_proporties_by_subcategory(req.params.id);
-    res.send('produkt o id:' + req.params.id);
+    res.render('product', {
+        id: req.params.id,
+        editable: false, //widok dla admina z edycją
+        name: "Wasteland",
+        price: 50,
+        imgurl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.angrymetalguy.com%2Fwp-content%2Fuploads%2F2018%2F09%2FPress_Cover_01-1.jpg&f=1&nofb=1",
+        desc: "I'm your lie\n\
+                I am your pretending\n\
+                I'm the cause\n\
+                Of your shame and anger<br>\
+                \n\
+                I'm your crime\n\
+                Swept under the carpet\n\
+                Your vanity\n\
+                With all consequences",
+        display: [
+            { href: "https://www.youtube.com/watch?v=dQw4w9WgXcQ", name: "Komp", imgurl: "images/test.png", desc: "Lorem Ipsum Lorem Ipsum  Lorem Ipsum", price: 5000 },
+            { href: "https://www.youtube.com/watch?v=dQw4w9WgXcQ", name: "Komp", imgurl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.AFt6jAmiSg_OdO67WkA0CgHaD3%26pid%3DApi&f=1", desc: "Lorem Ipsum", price: 5000 },
+            { href: "https://www.youtube.com/watch?v=dQw4w9WgXcQ", name: "Komp", imgurl: "images/test.png", desc: "Lorem Ipsum", price: 5000 },
+            { href: "https://www.youtube.com/watch?v=dQw4w9WgXcQ", name: "Komp", imgurl: "images/test.png", desc: "Lorem Ipsum Lorem Ipsum  Lorem Ipsum", price: 5000 }
+        ]
+    });
 });
