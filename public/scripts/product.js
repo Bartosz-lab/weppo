8c13fc7d066b947d0081c968acdc2d65a37fa951
Xelonidas <mipokoj@gmail.com>
2022-01-29 16:33:43 +0100

    Lepszy alert box

--- public/scripts/product.js
+++ public/scripts/product.js
@@ -9,12 +9,23 @@ async function postData(url = '', data = {}) {
     return response.json();
 }
 
+alertBox = document.getElementById('alert')
+alertBox.style.visibility = "hidden";
+
 function Add_to_basket() {
     postData('/basket/add', {
         id: document.getElementById('product_id').value,
         quantity: document.getElementById('quantity').value
     }).then(data => {
-        alert(data.Response);
+        alertBox.style.visibility = "visible";
+        alertBox.style.borderColor = (data.Response[0] == '0') ? "var(--green)" : "var(--red)";
+        alertBox.innerHTML = (data.Response[0] == '0') ? 
+          "Poprawnie dodano wybrany produkt do koszyka" : 
+          "Wystąpił problem z dodaniem produktu do koszyka";
+
+        setTimeout(_ => {
+          alertBox.style.visibility = "hidden";
+        }, 3000);
     });
 
 };
