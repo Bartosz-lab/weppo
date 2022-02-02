3c5d993138a6d09c8c5607ae2a23a0a4541b8b26
Xelonidas <mipokoj@gmail.com>
2022-02-02 10:28:53 +0100

    user administration pt2

--- routes/user.js
+++ routes/user.js
@@ -19,6 +19,19 @@ router.get('/', auth.restrict_login, auth.restrict_role(Role.Admin), async (req,
     }
 });
 
+router.post('/promote', auth.restrict_login, auth.restrict_role(Role.Admin), async (req, res) => {
+    try {
+        
+        await database.add_role_to_user(req.body.id, Role.Admin);
+        const users = await database.get_users();
+        res.render('users-administration', {users: users});
+
+    } catch (err) {
+        req.session.error = err.message;
+        res.redirect('/error');
+    }
+});
+
 router.get('/:id', auth.restrict_login, auth.restrict_role(Role.Admin), async (req, res) => {
     try {
         const user = await database.get_user_info_by_id(req.params.id);
