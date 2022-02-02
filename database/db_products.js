e9cc7effe37e279fef3cbfa39df66feee7b78456
pumaplx3 <pumaplx3@gmail.com>
2022-02-02 18:27:07 +0100

    Add product umie dodawac filtry produktom

--- database/db_products.js
+++ database/db_products.js
@@ -142,19 +142,30 @@ module.exports.get_product_by_id = get_product_by_id;
  * @return {} nothing
  */
  async function add_product (Product) {
-    try {
+   try {
         const result = await Pool.query(
             `INSERT INTO products (id, name, subcat_id, price, descr, brand, photo_url) VALUES 
             (DEFAULT, $1, $2, $3, $4, $5, $6) RETURNING id;`,
             [Product.name, Product.subcat_id, Product.price, Product.desc, Product.brand, Product.imgurl]);
         if (!result.rows[0]) throw new Error('7. Database Error');
-        else return result.rows[0].id;
+
+        let i = 0;
+        while (Product.params[i]) {
+          filter_name = Product.params[i].key;
+          option_value = Product.params[i].value;
+          const filter_option_id = await Pool.query (`SELECT option_id FROM widok11 where filter_name = $1 AND option_value = $2;`, [filter_name, option_value])
+          
+          if (filter_option_id.rows[0].option_id) {
+            const result2 = await Pool.query (`INSERT INTO products_to_filters (product_id, filter_option_id) VALUES ($1,$2);`,[result.rows[0].id, filter_option_id.rows[0].option_id]);
+          }
+          i++;
+        }
+        return result.rows[0].id;
     } catch (err) {
         throw_my_error(err);
     }
 }
 module.exports.add_product = add_product;
-
 
 /**
  * Update product info
